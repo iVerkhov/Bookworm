2ce03dc4f8c6aa3ed250352eb5922d853efb1fe3
iVerkhov <vrkhv@iCloud.com>
2023-09-11 12:27:40 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Игорь Верхов on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
